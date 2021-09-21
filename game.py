af746e5a806966802c55c656c94fb2acc10e8a50
Jake Kim <developjake@gmail.com>
2021-09-21 14:28:48 -0700

    upload project

---
+++ game.py
@@ -0,0 +1,445 @@
+from graphics import *
+
+class Round():
+
+    def __init__(self, window, difficulty):
+        self.win = window
+
+        # set difficulty for distribution
+        mode = {'easy' : 20, 'medium' : 10, 'hard' : 8, 'test' : 2}
+        self.distribution = mode[difficulty.lower()]
+        self.spacing = self.win.getWidth()/self.distribution
+
+        # related to lines
+        self.lineList = self.__generateLines()
+        self.slotList = self.__generateSlots()
+
+        # conditions of the round
+        self.numTurns = 0
+        self.running = True
+
+        # extra cond
+        self.currentRowIndex = 0
+        self.currentColumnIndex = 0
+        self.currentPlayerId = 0
+
+    # generate
+    def __generateLines(self):
+        li = []
+
+        for x in range(self.distribution - 1):
+            # get horizontal line end points
+            hPoint1 = Point(self.spacing, self.spacing * (x + 1))
+            hPoint2 = Point(self.win.getWidth() - self.spacing, self.spacing * (x + 1))
+            
+            # get vertical line end points
+            vPoint1 = Point(self.spacing * (x + 1), self.spacing)
+            vPoint2 = Point(self.spacing * (x + 1), self.win.getWidth() - self.spacing)
+
+            # create line
+            hLine = Line(hPoint1, hPoint2)
+            vLine = Line(vPoint1, vPoint2)
+
+            # set color to white
+            hLine.setOutline(color_rgb(156,116,43))
+            vLine.setOutline(color_rgb(156,116,43))
+
+            # add to line list
+            li.append(hLine)
+            li.append(vLine)
+        
+        return li
+    
+    
+    def __generateSlots(self):
+        li = []
+
+        for i in range(self.distribution - 1):
+            row = []
+
+            for j in range(self.distribution - 1):
+                x = Slot(self.spacing*(j + 1), self.spacing*(i + 1))
+
+                row.append(x)
+
+            li.append(row)
+        
+        return li
+    
+
+    def drawLines(self):   
+        for x in range((self.distribution - 1)*2):
+            self.lineList[x].draw(self.win)
+
+    # find closet slot to add
+    def findSlot(self, xCord, yCord):  
+        column = 0
+        for i in self.slotList[0]:
+            
+            if abs(i.getX() - xCord) < abs(self.slotList[0][column].getX() - xCord):
+                column = self.slotList[0].index(i)
+        
+        closetYDistance = self.win.getWidth()
+        chosenSlot = None
+        row = 0
+
+        for i in self.slotList:
+            yDistance = abs(i[column].getY() - yCord)
+            if yDistance <= closetYDistance:
+                closetYDistance = yDistance
+                row = self.slotList.index(i)
+                chosenSlot = i[column]
+        
+        # set postions
+        self.currentRowIndex = row
+        self.currentColumnIndex = column
+
+        return chosenSlot
+        
+    # game states
+    def nextTurn(self): self.numTurns+=1
+
+
+    def isRunning(self): return self.running
+
+
+    def isFull(self):
+        
+        if self.numTurns == len(self.slotList)**2:
+            return True
+            
+        else:
+            return False
+    
+    # get
+    def getSpacing(self): return self.spacing
+
+
+    def getTurns(self): return self.numTurns
+
+    # end round
+    def hasFiveConnected(self, slot):
+
+        def changeColor(listOfSlots):
+            if slot.getId() == 1:
+                color = "black"
+            else:
+                color = "white"
+
+            for i in listOfSlots:
+                dot = i.getDot()
+                dot.undraw()
+                dot.setOutline(color)
+                dot.draw(self.win)
+        
+        # preset cond
+        numConnected = 0
+        slotsConnected = []
+        row = self.slotList[self.currentRowIndex]
+        
+        # count right horizontal
+        for i in range(1, 5):
+            if self.currentColumnIndex + i < len(row):
+                nextSlot = row[self.currentColumnIndex + i]
+
+                if nextSlot.getId() == slot.getId():
+                    slotsConnected.append(nextSlot)
+                    numConnected+=1
+                else:
+                    break
+            else:
+                break
+        
+        # check if connected
+        if numConnected == 4:
+            slotsConnected.append(slot)
+            changeColor(slotsConnected)
+            return True
+        
+        # count left horizontal
+        for i in range(1, 5):
+            if self.currentColumnIndex - i >= 0:
+                nextSlot = row[self.currentColumnIndex - i]
+
+                if nextSlot.getId() == slot.getId():
+                    slotsConnected.append(nextSlot)
+                    numConnected+=1
+                else:
+                    break
+            else:
+                break
+        
+        # check if connected
+        if numConnected == 4:
+            slotsConnected.append(slot)
+            changeColor(slotsConnected)
+            return True
+
+        # reset
+        numConnected = 0
+        slotsConnected = []
+        
+        # count up vertical
+        for i in range(1, 5):
+            if self.currentRowIndex - i >= 0:
+                row = self.slotList[self.currentRowIndex - i]
+                nextSlot = row[self.currentColumnIndex]
+
+                if nextSlot.getId() == slot.getId():
+                    slotsConnected.append(nextSlot)
+                    numConnected+=1
+                else:
+                    break
+            else:
+                break
+        
+       # check if connected
+        if numConnected == 4:
+            slotsConnected.append(slot)
+            changeColor(slotsConnected)
+            return True
+        
+        # count down vertical
+        for i in range(1, 5):
+            if self.currentRowIndex + i < len(self.slotList):
+                row = self.slotList[self.currentRowIndex + i]
+                nextSlot = row[self.currentColumnIndex]
+        
+                if nextSlot.getId() == slot.getId():
+                    slotsConnected.append(nextSlot)
+                    numConnected+=1
+                else:
+                    break
+            else:
+                break
+        
+        # check if connected
+        if numConnected == 4:
+            slotsConnected.append(slot)
+            changeColor(slotsConnected)
+            return True
+
+        # reset 
+        numConnected = 0
+        slotsConnected = []
+
+        # count diagonal right up
+        for i in range(1, 5):
+            if self.currentColumnIndex + i < len(row) and self.currentRowIndex - i >= 0:
+                row = self.slotList[self.currentRowIndex - i]
+                nextSlot = row[self.currentColumnIndex + i]
+        
+                if nextSlot.getId() == slot.getId():
+                    slotsConnected.append(nextSlot)
+                    numConnected+=1
+                else:
+                    break
+            else:
+                break
+        
+        # check if connected
+        if numConnected == 4:
+            slotsConnected.append(slot)
+            changeColor(slotsConnected)
+            return True
+    
+        # count diagonal down left
+        for i in range(1, 5):
+            if self.currentColumnIndex - i >= 0 and self.currentRowIndex + i < len(self.slotList):
+                row = self.slotList[self.currentRowIndex + i]
+                nextSlot = row[self.currentColumnIndex - i]
+        
+                if nextSlot.getId() == slot.getId():
+                    slotsConnected.append(nextSlot)
+                    numConnected+=1
+                else:
+                    break
+            else:
+                break
+        
+        # check if connected
+        if numConnected == 4:
+            slotsConnected.append(slot)
+            changeColor(slotsConnected)
+            return True
+        
+        # reset
+        numConnected = 0
+        slotsConnected = []
+        
+        # count diagonal left up
+        for i in range(1, 5):
+            if self.currentColumnIndex - i >= 0 and self.currentRowIndex - i >= 0:
+                row = self.slotList[self.currentRowIndex - i]
+                nextSlot = row[self.currentColumnIndex - i]
+        
+                if nextSlot.getId() == slot.getId():
+                    slotsConnected.append(nextSlot)
+                    numConnected+=1
+                else:
+                    break
+            else:
+                break
+        
+        # check if connected
+        if numConnected == 4:
+            slotsConnected.append(slot)
+            changeColor(slotsConnected)
+            return True
+        
+        # count diagonal down right
+        for i in range(1, 5):
+            if self.currentColumnIndex + i < len(row) and self.currentRowIndex + i < len(self.slotList):
+                row = self.slotList[self.currentRowIndex + i]
+                nextSlot = row[self.currentColumnIndex + i]
+        
+                if nextSlot.getId() == slot.getId():
+                    slotsConnected.append(nextSlot)
+                    numConnected+=1
+                else:
+                    break
+            else:
+                break
+        
+        # check if connected
+        if numConnected == 4:
+            slotsConnected.append(slot)
+            changeColor(slotsConnected)
+            return True
+        
+        return False
+
+    def end(self): self.running = False
+    
+
+
+class Player():
+
+    def __init__(self, playerId, color):
+        self.playerId = playerId
+        self.color = color
+    
+
+    def getColor(self): return self.color
+
+
+    def getId(self): return self.playerId
+
+class Slot():
+
+    def __init__(self, x : int, y : int):
+        self.x = x
+        self.y = y
+        self.slotOccupied = False
+
+        # id is a single interger : 1 & 2 | 0 means there is no ID = no occupant of the slot
+        self.occupantId = 0
+        self.dot = None
+
+
+    def occupy(self, player): 
+        self.occupantId = player.getId()
+        self.slotOccupied = True
+
+
+    def getX(self): return self.x
+
+
+    def getY(self): return self.y
+
+
+    def getId(self): return self.occupantId
+
+    
+    def getDot(self): return self.dot
+    
+
+    def isOccupied(self): return self.slotOccupied
+
+    # linking the Circle object to the specfied Slot object
+    def addDotToSlot(self, dot): self.dot = dot
+    
+class Button():
+
+    def __init__(self, p1, p2):
+        self.rec = Rectangle(p1, p2);
+        self.text = None
+
+        self.leftX = self.rec.getP1().getX()
+        self.rightX = self.rec.getP2().getX()
+
+        self.topY = self.rec.getP1().getY()
+        self.botY = self.rec.getP2().getY()
+
+        self.centerPoint = self.rec.getCenter()
+
+
+    def ifPressed(self, p):
+        
+        insideX = False
+        insideY = False
+
+        if p.getX() > self.leftX and p.getX() < self.rightX:
+            insideX = True
+        
+        if p.getY() > self.topY and p.getY() < self.botY:
+            insideY = True
+
+        if insideX and insideY:
+            return True
+        else:
+            return False
+
+
+    def draw(self, window):
+        if self.text is not None:
+            self.rec.draw(window)
+            self.text.draw(window)
+        else:
+            self.rec.draw(window)
+
+
+    def undraw(self):
+        if self.text == None:
+            self.rec.undraw()
+            
+        else:
+            self.rec.undraw()
+            self.text.undraw()
+    
+
+    def setText(self, strLine, size):
+        self.text = Text(self.centerPoint, strLine)
+        self.text.setSize(size)
+    
+
+    def setTextColor(self, colors):
+        self.text.setTextColor(colors)
+
+
+    def setOutline(self, colors):
+        self.rec.setOutline(colors)
+
+
+    def setFill(self, colors):
+        self.rec.setFill(colors)
+
+    
+
+    
+    
+
+
+      
+    
+
+    
+
+
+    
+
+        
+    
+
+
+        
---
+++ main.py
@@ -0,0 +1,94 @@
+from game import *
+from graphics import *
+import buttonlist
+
+
+
+# preset conditions
+print("""
+                            [CONNECT 5]
+[How To Play]: Just like the title, you need to 'Connect 5' linear dots
+in order to win! The 5 dots can be horizontal, vertical, and diagonal.
+You will need 1 other player to play with. You will take turns placing 
+dots on the intersections of lines on the graph and compete against eachother to be 
+the first one to connect 5 dots. 
+""")
+
+
+print('[Difficulty] --> [EASY](19x19) - [MEDIUM](9x9) - [HARD](7x7)')
+# options for difficulty input = "easy" - "medium" - "hard" [Capitalization does not matter]
+difficulty = input("What is your preferred difficulty (ex: 'easy'): ")
+print()
+player1 = Player(1, color_rgb(246, 242, 235))
+player2 = Player(2, color_rgb(64, 62, 58))
+winSize = 700
+
+# window setup
+win = GraphWin(title='Connect 5', width=winSize, height=winSize)
+win.setBackground(color_rgb(247,215,157))
+print(type(win))
+
+# buttons setup
+s_button = buttonlist.getStart(winSize)
+s_button.draw(win)
+
+while True:
+
+    if s_button.ifPressed(win.getMouse()):
+        s_button.undraw()
+        break
+    else:
+        pass
+
+
+# game setup
+round = Round(win, difficulty)
+round.drawLines()
+dotRadius = round.getSpacing()/3
+players = [player1, player2]
+
+# round
+while round.isRunning():
+
+    # find player
+    player = players[round.getTurns() % 2]
+    print(f"[Turn#{round.getTurns() + 1}] Player#{players.index(player) + 1}'s turn! [{player.getColor()}]")
+    print()
+
+    # get mouse coords
+    mouse = win.getMouse()
+    xCord = mouse.getX()
+    yCord = mouse.getY()
+
+    # find slot
+    slot = round.findSlot(xCord, yCord)
+
+    if slot.isOccupied():
+        print("occupied")
+        
+    else:
+        
+        # draw dot on a specific slot
+        dot = Circle(Point(slot.getX(), slot.getY()), dotRadius) 
+        dot.setOutline(player.getColor())
+        dot.setFill(player.getColor())
+
+        slot.occupy(player)
+        slot.addDotToSlot(dot)
+        dot.draw(win)
+        
+        round.nextTurn()
+
+    # has five connected
+    if round.hasFiveConnected(slot):
+        print(f"Player#{player.getId()} Wins! Click anywhere to END")
+        round.end()
+        
+    # if full
+    if round.isFull():
+        round.end()
+
+# wait for window to close
+win.getMouse()
+win.close()
+
